14ae9ec1810ea78f6abf1fdc212fe21276a95782
Estanislao Crivos <estanislaocrivos@gmail.com>
2025-08-06 14:55:45 -0300

    Updates.

--- test/test_main.c
+++ test/test_main.c
@@ -1,6 +1,10 @@
-#include "unity.h"
+#include "../build/vendor/unity/src/unity.h"
+
+#include "../inc/lib.h"
 
 void test(void)
 {
+    TEST_ASSERT_EQUAL(-1, lib_function(0));
+    TEST_ASSERT_EQUAL(4, lib_function(2));
     TEST_ASSERT_EQUAL(1, 1);
 }
